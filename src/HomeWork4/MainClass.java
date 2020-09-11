049de294419e23f8259c2a3366118fbd4156bab8
Max <Maximkot@mail.ru>
2020-09-11 06:08:27 +0300

    HW4

---
+++ src/HomeWork4/MainClass.java
@@ -0,0 +1,156 @@
+package HomeWork4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainClass {
+    // Задание
+    // 1. Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+    //2. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
+    //3. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+    static final int SIZE = 3; // Размер поля
+    static final int DOT_TO_WIN = 3; // Кол-во фишек для выиграша
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили! Поздравляем!");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Компьютер победил.");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+
+    }
+
+    public static void initMap() { // Создания поля
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() { // Отображение поля
+        System.out.print("  ");
+        for (int i = 1; i <= map.length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < map.length; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.printf("%c ", map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void humanTurn() { // Запрос у пользователя куда поставвить крестик
+        int x;
+        int y;
+
+        do {
+            System.out.println("input X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int y, int x) { // Проверка пустое ли поле по координатам
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+//        if (map[y][x] == DOT_EMPTY) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+    }
+
+    public static void aiTurn() { // Ход ПК
+        int x;
+        int y;
+
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isFull() { // Проверка все ли поля заняты
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkWin(char c) { // Проверка есть ли победа
+        if (map[0][0] == c && map[0][1] == c && map[0][2] == c) {
+            return true;
+        }
+        if (map[1][0] == c && map[1][1] == c && map[1][2] == c) {
+            return true;
+        }
+        if (map[2][0] == c && map[2][1] == c && map[2][2] == c) {
+            return true;
+        }
+
+        if (map[0][0] == c && map[1][0] == c && map[2][0] == c) {
+            return true;
+        }
+        if (map[0][1] == c && map[1][1] == c && map[2][1] == c) {
+            return true;
+        }
+        if (map[0][2] == c && map[1][2] == c && map[2][2] == c) {
+            return true;
+        }
+
+        if (map[0][0] == c && map[1][1] == c && map[2][2] == c) {
+            return true;
+        }
+        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) {
+            return true;
+        }
+
+        return false;
+    }
+}
