be4c670e9ed38ab512346b5b0b9db525b904697c
Max <Maximkot@mail.ru>
2020-09-11 21:12:07 +0300

    HW5

--- src/HomeWork5/MainClass.java
+++ src/HomeWork5/MainClass.java
@@ -1,6 +1,25 @@
 package HomeWork5;
 
 public class MainClass {
-
-
+    /* * Создать массив из 5 сотрудников
+    Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+     persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+     persArray[1] = new Person(...);
+     ...
+     persArray[4] = new Person(...);
+ * С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+     */
+    public static void main(String[] args) {
+        Person[] persArr = new Person[5];
+        persArr[0] = new Person("Ivanov Ivan", "Engineer", "ivi@mailbox.com", "892312312", 30000, 30);
+        persArr[1] = new Person("Sidorov Petr", "CEO", "ss@mailbox.com", "892312312", 130000, 54);
+        persArr[2] = new Person("Ivanov Serg", "Engineer", "van@mailbox.com", "892312312", 30000, 41);
+        persArr[3] = new Person("Petrov Andrey", "Programmer", "in@mailbox.com", "892312312", 60000, 40);
+        persArr[4] = new Person("Putin Alex", "Project manager", "ttt@mailbox.com", "892312312", 90000, 60);
+        for (Person person : persArr) {
+        if (person.getAge()>40) {
+            person.info();
+        }
+        }
+    }
 }
--- src/HomeWork5/Person.java
+++ src/HomeWork5/Person.java
@@ -1,18 +1,40 @@
 package HomeWork5;
 
 public class Person {
-   /* * Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
-* Конструктор класса должен заполнять эти поля при создании объекта;
-* Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
-* Создать массив из 5 сотрудников
+    /* * Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+ * Конструктор класса должен заполнять эти поля при создании объекта;
+ * Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+ * Создать массив из 5 сотрудников
 
-Пример:
-Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
-persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
-persArray[1] = new Person(...);
-...
-persArray[4] = new Person(...);
-* С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
-*/
+ Пример:
+ Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+ persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+ persArray[1] = new Person(...);
+ ...
+ persArray[4] = new Person(...);
+ * С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+ */
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private int earn;
+    private int age;
 
+    public Person(String name, String position, String email, String phone, int earn, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.earn = earn;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.printf("ФИО: %s, Должность: %s, Почта: %s, Телефон: %s, Зарплата: %d, Возвраст: %d\n", name, position, email, phone, earn, age);
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
