eb6cc9e674f0afcb14f10afa7e2dda3278d0641d
Max <Maximkot@mail.ru>
2020-09-11 11:19:13 +0300

    HW4

--- src/HomeWork4/MainClass.java
+++ src/HomeWork4/MainClass.java
@@ -8,8 +8,8 @@ public class MainClass {
     // 1. Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
     //2. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
     //3. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
-    static final int SIZE = 3; // Размер поля
-    static final int DOT_TO_WIN = 3; // Кол-во фишек для выиграша
+    static final int SIZE = 6; // Размер поля
+    static final int DOT_TO_WIN = 4; // Кол-во фишек для выиграша
 
     static final char DOT_X = 'X';
     static final char DOT_O = 'O';
@@ -28,7 +28,7 @@ public class MainClass {
         while (true) {
             humanTurn();
             printMap();
-            if (checkWin(DOT_X)) {
+            if (newCheckWin(DOT_X)) {
                 System.out.println("Вы победили! Поздравляем!");
                 break;
             }
@@ -39,7 +39,7 @@ public class MainClass {
 
             aiTurn();
             printMap();
-            if (checkWin(DOT_O)) {
+            if (newCheckWin(DOT_O)) {
                 System.out.println("Компьютер победил.");
                 break;
             }
@@ -103,7 +103,10 @@ public class MainClass {
     public static void aiTurn() { // Ход ПК
         int x;
         int y;
-
+        // Выполнение блокировки
+        if (blockUser()) {
+            return;
+        }
         do {
             x = random.nextInt(SIZE);
             y = random.nextInt(SIZE);
@@ -123,7 +126,7 @@ public class MainClass {
         return true;
     }
 
-    public static boolean checkWin(char c) { // Проверка есть ли победа
+    public static boolean checkWin(char c) { // Проверка есть ли победа (старая версия)
         if (map[0][0] == c && map[0][1] == c && map[0][2] == c) {
             return true;
         }
@@ -151,6 +154,85 @@ public class MainClass {
             return true;
         }
 
+        return false;
+    }
+
+    public static boolean newCheckWin(char c) { // Проверка есть ли победа
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (checkCell(y, x, c)) { // Проверка ячейка
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean cellExist(int y, int x) { // Проверка существует ли ячейка с таким номером (или координаты вне поля)
+        if (y < 0 || x < 0 || y >= SIZE || x >= SIZE) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkCell(int y, int x, char c) { // Проверка есть ли в данной клетке выиграшная комбинация
+        boolean winH = true;
+        boolean winV = true;
+        boolean winD1 = true;
+        boolean winD2 = true;
+
+        for (int i = 0; i < DOT_TO_WIN; i++) {
+            // Проверка по горизонтали
+            if (cellExist(y, x + i)) {
+                if (map[y][x + i] != c) {
+                    winH = false;
+                }
+            } else {
+                winH = false;
+            }
+            // Проверка по вертикали
+            if (cellExist(y + i, x)) {
+                if (map[y + i][x] != c) {
+                    winV = false;
+                }
+            } else {
+                winV = false;
+            }
+            // Проверка по диагонали вверх
+            if (cellExist(y - i, x + i)) {
+                if (map[y - i][x + i] != c) {
+                    winD1 = false;
+                }
+            } else {
+                winD1 = false;
+            }
+            // Проверка по диагонали вниз
+            if (cellExist(y + i, x + i)) {
+                if (map[y + i][x + i] != c) {
+                    winD2 = false;
+                }
+            } else {
+                winD2 = false;
+            }
+        }
+        return winH || winV || winD1 || winD2;
+    }
+
+    public static boolean blockUser() { // Установка 0 для блокировки победы пользователя
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (isCellValid(y, x)) { // Проверка ячейки
+                    map[y][x] = DOT_X; // Временно устанавливаем Х (как будто пользователь походил)
+                    if (newCheckWin(DOT_X)) {
+                        map[y][x] = DOT_O; // Устанавливаем О
+                        return true;
+                    } else {
+                        map[y][x] = DOT_EMPTY; // Убираем временный Х
+                    }
+                }
+
+            }
+        }
         return false;
     }
 }
